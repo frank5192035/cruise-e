81194299e9df87e675e703f9c7fa47fd1a0725c2
Frank Hsiung <frank5192035@yahoo.com.tw>
2020-10-24 01:27:14 +0800

    可以測試到超音波回覆

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
 serial.writeLine("micro:bit Start ===>")
 basic.showString("Hello!")
+let stateEmit = 1
+let gotSonic = 0
 basic.forever(function () {
-	
+    if (stateEmit) {
+        serial.writeValue("stateEmit", gotSonic)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        gotSonic = pins.pulseIn(DigitalPin.P1, PulseValue.High)
+        while (gotSonic < 5) {
+            gotSonic = pins.pulseIn(DigitalPin.P1, PulseValue.High)
+        }
+        stateEmit = 0
+    } else {
+        serial.writeValue("stateEmit", 0)
+        stateEmit = 1
+    }
+    control.waitMicros(100000)
 })
