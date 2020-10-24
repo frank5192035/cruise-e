d77d279ec1fafb4c98df649205fe678921b224f4
Frank Hsiung <frank5192035@yahoo.com.tw>
2020-10-24 09:10:07 +0800

    避障的邏輯OK，但是循跡的功能似乎是硬體做固定的，會影響功能!

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+let random = 0
+pins.digitalWritePin(DigitalPin.P8, 0)
+pins.digitalWritePin(DigitalPin.P11, 0)
+pins.digitalWritePin(DigitalPin.P12, 0)
 serial.writeLine("micro:bit Start ===>")
 basic.showString("Hello!")
 let stateEmit = 1
 let gotSonic = 0
+pins.servoSetPulse(AnalogPin.P13, 20000)
+pins.servoSetPulse(AnalogPin.P15, 20000)
 basic.forever(function () {
     if (stateEmit) {
         serial.writeValue("stateEmit", gotSonic)
@@ -13,9 +19,29 @@ basic.forever(function () {
             gotSonic = pins.pulseIn(DigitalPin.P1, PulseValue.High)
         }
         stateEmit = 0
+        control.waitMicros(300)
+        if (gotSonic < 300) {
+            random = randint(1, 10)
+            if (random > 5) {
+                pins.digitalWritePin(DigitalPin.P15, 0)
+                control.waitMicros(500000)
+                pins.servoSetPulse(AnalogPin.P16, 20000)
+                control.waitMicros(3000000)
+            } else {
+                pins.digitalWritePin(DigitalPin.P13, 0)
+                control.waitMicros(500000)
+                pins.servoSetPulse(AnalogPin.P14, 20000)
+                control.waitMicros(3000000)
+            }
+        }
     } else {
         serial.writeValue("stateEmit", 0)
         stateEmit = 1
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        control.waitMicros(500000)
+        pins.servoSetPulse(AnalogPin.P13, 20000)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        control.waitMicros(500000)
+        pins.servoSetPulse(AnalogPin.P15, 20000)
     }
-    control.waitMicros(100000)
 })
